c5aa5f371709fccc7981a3f513f828e631ce443e
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-05-21 23:38:44 -0300

    resolución ejercicio 1

--- clase_8/ejercicios/ejercicio1.js
+++ clase_8/ejercicios/ejercicio1.js
@@ -12,3 +12,14 @@ Requisitos:
 • Utiliza un operador aritmético para calcular el doble del número
 */
 
+// Requerir prompt-sync para poder usar prompt en Node.js
+const prompt = require('prompt-sync')();
+
+//1- Solicitar al usuario que ingrese un número cualquiera
+let numero = parseFloat(prompt('Ingrese un número: '));
+
+//2- Calcular el doble de ese número
+let doble = numero * 2;
+
+//3- Mostrar el resultado en la consola
+console.log(`El doble de ${numero} es ${doble}`);
