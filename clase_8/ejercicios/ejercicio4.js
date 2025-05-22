cb441ff671c8968f2b7559b51f7a330fe1f3e721
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-05-21 23:52:57 -0300

    resolucion ejercicio 4

--- clase_8/ejercicios/ejercicio4.js
+++ clase_8/ejercicios/ejercicio4.js
@@ -10,3 +10,23 @@ Requisitos:
 • Utiliza el operador módulo (%) para comprobar si el número es divisible por 2.
 • Usa una estructura if-else para mostrar el mensaje adecuado.
 */
+
+// Requerir promp-sync para poder usar prompt en node.js
+const prompt = require('prompt-sync')();    
+
+// 1- Pedir al usuario que ingrese un número
+const numero = parseInt(prompt('Ingrese un número entero: '));  
+
+// 2- Verificación del número con estructura de control de flujo:
+// - Numero par = divisible por 2 -> numero % 2 === 0;
+// - Numero impar = no divisible por 2 -> numero % 2 !== 0; 
+
+if (numero % 2 === 0) {
+
+    // 3-a- Mostrar mensaje indicando si el número es par
+    console.log(`El número ${numero} es par.`);
+} else {
+     // 3-b- Mostrar mensaje indicando si el número es par
+    console.log(`El número ${numero} es impar.`);
+}   
+
