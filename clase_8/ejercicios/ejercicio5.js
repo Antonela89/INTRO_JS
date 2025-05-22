5e8ed5b3ef2f2b7e67bdc2ea7b33018f9316a243
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-05-21 23:57:02 -0300

    resolucion ejercicio 5

--- clase_8/ejercicios/ejercicio5.js
+++ clase_8/ejercicios/ejercicio5.js
@@ -9,3 +9,15 @@ Requisitos:
 • Utiliza prompt() para recibir el nombre del usuario.
 • Muestra un mensaje utilizando concatenación de cadenas
 */
+
+// Requerir promp-sync para poder usar prompt en node.js
+const prompt = require('prompt-sync')();
+
+// 1- Pedir al usuario que ingrese su nombre
+const nombre = prompt('Ingrese su nombre: ');
+
+// 2- Mostrar un saludo personalizado
+console.log('Hola ' + nombre + ', ¡bienvenido/a!');
+
+// 2-a- Usando template literals
+//console.log(`Hola, ${nombre}! Bienvenido/a al programa!`);
