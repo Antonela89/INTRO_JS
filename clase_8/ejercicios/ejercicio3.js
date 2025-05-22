fabe68b7a242432cdc85ca0a03fd86ecc2544db1
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-05-21 23:49:31 -0300

    resolucion ejercicio 3

--- clase_8/ejercicios/ejercicio3.js
+++ clase_8/ejercicios/ejercicio3.js
@@ -11,3 +11,18 @@ Requisitos:
 • Utiliza parseFloat() para convertir las entradas a números.
 • Almacena los resultados en variables adecuadas y muestra el resultado.
 */
+
+// Requerir promp-sync para poder usar prompt en node.js
+const prompt = require('prompt-sync')();
+
+// 1- Pedir al usuario que ingrese primer número
+const numero1 = parseFloat(prompt('Ingrese el primer número: '));
+
+// 2- Pedir al usuario que ingrese segundo número
+const numero2 = parseFloat(prompt('Ingrese el segundo número: '));
+
+// 3- Sumar ambos números
+const suma = numero1 + numero2;
+
+// 4- Mostrar el resultado de la suma
+console.log(`La suma de ${numero1} y ${numero2} es: ${suma}`);
